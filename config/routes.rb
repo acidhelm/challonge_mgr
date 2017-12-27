cfdd79700f77d169d3c140d25b7154a6e11eecd9
Michael Dunn <acidhelm@gmail.com>
2017-12-26 17:57:53 -0800

    Add a root line to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    root "users#index"
     resources :users
+    resources :tournaments
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
