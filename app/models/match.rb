b9fe0b8dd35fb62c89beb9cb89c740294f2e28b6
Michael Dunn <acidhelm@gmail.com>
2018-01-02 13:02:00 -0800

    Add some scopes to the Match class to make queries nicer

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,12 @@
 class Match < ApplicationRecord
     belongs_to :tournament
+
+    scope :complete?, -> { where(state: "complete") }
+    scope :has_team?, ->(id) { where("team1_id = ? OR team2_id = ?", id, id) }
+    scope :winner?, ->(id) { complete?.where(winner_id: id) }
+    scope :loser?, ->(id) { complete?.has_team?(id).where.not(winner_id: id) }
+
+    def complete?
+        return state == "complete"
+    end
 end
