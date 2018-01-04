f00d70db6013baf71ade5d71dd5fa20edc6899ac
Michael Dunn <acidhelm@gmail.com>
2018-01-04 01:13:49 -0800

    Show completed and not-completed matches separately in the Tournament#show view. Remove some of the content that the scaffold generator generated.

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,6 +5,8 @@ class Match < ApplicationRecord
     scope :has_team?, ->(id) { where("team1_id = ? OR team2_id = ?", id, id) }
     scope :winner?, ->(id) { complete?.where(winner_id: id) }
     scope :loser?, ->(id) { complete?.has_team?(id).where.not(winner_id: id) }
+    scope :upcoming, -> { where.not(state: "complete").order(suggested_play_order: :asc) }
+    scope :completed, -> { complete?.order(suggested_play_order: :asc) }
 
     def complete?
         return state == "complete"
