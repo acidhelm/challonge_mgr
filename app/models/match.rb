53d8b7da2bdcc4a89e26d2e271ed16d623b82aed
Michael Dunn <acidhelm@gmail.com>
2018-01-11 02:30:10 -0800

    Remove old Match accessors that dealt with team1 and team2

--- app/models/match.rb
+++ app/models/match.rb
@@ -153,30 +153,6 @@ class Match < ApplicationRecord
         return tournament.gold_on_left ? "Blue" : "Gold"
     end
 
-    def team1_won?
-        return complete? && team1_id == winner_id
-    end
-
-    def team2_won?
-        return complete? && team2_id == winner_id
-    end
-
-    def team1_score
-        return scores_csv.blank? ? 0 : scores_csv.partition(",")[0].split("-")[0].to_i
-    end
-
-    def team2_score
-        return scores_csv.blank? ? 0 : scores_csv.partition(",")[0].split("-")[1].to_i
-    end
-
-    def team1_name
-        return tournament.teams.find_by_challonge_id(team1_id).name
-    end
-
-    def team2_name
-        return tournament.teams.find_by_challonge_id(team2_id).name
-    end
-
     # The `suggested_play_order` field ranges from 1 to N, which is useful for
     # identifying matches in the UI.  I don't want to refer to `suggested_play_order`
     # in views, though, even it is (currently) equal to the match number.
