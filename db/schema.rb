f20f9d8d0568296abc7ab6723cd78d40dc28a6cd
Michael Dunn <acidhelm@gmail.com>
2018-01-27 21:39:40 -0800

    Add "password_digest" to the Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119192407) do
+ActiveRecord::Schema.define(version: 20180128044754) do
 
   create_table "matches", force: :cascade do |t|
     t.integer "tournament_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180119192407) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
