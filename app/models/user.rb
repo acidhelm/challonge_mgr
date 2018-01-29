0dac2f40daa067d279bf450d56c77588cd70278f
Michael Dunn <acidhelm@gmail.com>
2018-01-28 22:00:53 -0800

    Use attr_encrypted to encrypt User#api_key in the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
     has_many :tournaments, dependent: :destroy
 
     validates :user_name, presence: true
-    validates :api_key, presence: true
     validates :password, presence: true, allow_nil: true
-
     has_secure_password
+
+    # The `attribute` line is necessary to fix a deprecation warning. The change
+    # comes from https://github.com/attr-encrypted/attr_encrypted/pull/263/files#diff-a469a309f6fa5bb5d23e5d0c2c8de65fR54
+    attribute :api_key
+    attr_encrypted :api_key, key: ENV["ATTR_ENCRYPTED_KEY"]
 
     # Returns a string containing the hash of `str`.
     def self.digest(str)
