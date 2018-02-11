6831d32a95e29f2523e0926ed1cfe7c626fe72cd
Michael Dunn <acidhelm@gmail.com>
2018-02-11 12:28:25 -0800

    Add a PowerShell script that polls the team name URLs The script writes the team names to gold.txt and blue.txt on the desktop. The streaming software can be set to read the names from those files.

---
+++ scripts/download_names.ps1
@@ -0,0 +1,35 @@
+# If you get an error about scripts being disabled, run:
+#   Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
+# That will let scripts run for that PowerShell session only.
+# You can also use `-Scope CurrentUser` to make the change permanent,
+# but that has security implications, because _all_ scripts can run,
+# including malicious ones.
+
+# TODO: Change this to your server's URL
+$server = "http://YOUR-SERVER-NAME.example.com"
+
+# TODO: Change this to your tournament's name.  This is the part of your
+#       tournament's URL after "challonge.com/".
+$tournament = "YOUR-TOURNAMENT-NAME"
+
+$desktop = [Environment]::GetFolderPath("desktop")
+$gold_name_url = "$server/view/$tournament/gold"
+$blue_name_url = "$server/view/$tournament/blue"
+$gold_name_file = $(Join-Path $desktop "gold.txt")
+$blue_name_file = $(Join-Path $desktop "blue.txt")
+
+if ( ( $server -cmatch "YOUR-" ) -or ( $tournament -cmatch "YOUR-" ) )
+{
+    "Before you can use this script, you must edit it and set `$server and `$tournament"
+    "to match your setup.  See the TODO comments at the top of the file."
+    exit
+}
+
+"Downloading team names from $server/view/$tournament. Press Ctrl+C to stop."
+
+while (1)
+{
+    Invoke-WebRequest $gold_name_url -OutFile $gold_name_file 
+    Invoke-WebRequest $blue_name_url -OutFile $blue_name_file 
+    sleep 15
+}
