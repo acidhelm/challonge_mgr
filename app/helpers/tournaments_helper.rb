7611cc217d2a3c66d90bd6642cef12f3a6b68284
Michael Dunn <acidhelm@gmail.com>
2018-03-26 19:28:11 -0700

    Tweaks to the Slack message about the on-deck match If one team is TBD, always show that team second.  So the message is always "Team Foo vs. TBD", never "TBD vs. Team Foo". Don't show the message if both teams are TBD, which happens for the first match in the final of a double-elim tourney.

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -4,8 +4,20 @@ module TournamentsHelper
                 " #{match.team_name(:left)} vs. #{match.team_name(:right)}." \
 
         if next_match
-            msg << " The on-deck match is #{next_match.team_name(:left) || "TBD"} vs." \
-                     " #{next_match.team_name(:right) || "TBD"}."
+            left_team_name = next_match.team_name(:left)
+            right_team_name = next_match.team_name(:right)
+
+            # If both team names are `nil`, then don't show the on-deck match
+            # message.  This happens for the first match of the finals in a
+            # double-elimination tournament.
+            if left_team_name || right_team_name
+                # If one team is TBD, always show that one second.
+                first_team_name = left_team_name || right_team_name
+                second_team_name = left_team_name.nil? ? nil : right_team_name
+                second_team_name ||= "TBD"
+
+                msg << " The on-deck match is #{first_team_name} vs. #{second_team_name}."
+            end
         end
 
         notify_slack(msg, tournament.slack_notifications_channel)
