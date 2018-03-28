e1f746ef0a21aee290dcf82c38669c9ccf35276d
Michael Dunn <acidhelm@gmail.com>
2018-03-28 04:24:16 -0700

    Add strings to en.rb for User views

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -13,6 +13,31 @@
                 password_accesskey: "p",
                 log_in_button: "Log in"
             }
-        }
+        },
+        users: {
+            show: {
+                user_name: "User name:",
+                api_key: "API key:",
+                subdomain: "Subdomain:",
+                edit_user: "Edit this user's settings",
+                view_tournaments: "View this user's tournaments"
+            },
+            edit: {
+                page_title: "Edit settings for %{user_name}",
+                page_header: "Edit settings for %{user_name}"
+            },
+            form: {
+                errors_list_header: {
+                    one: "1 error prevented this user from being saved:",
+                    other: "%{count} errors prevented this user from being saved:"
+                },
+                api_key: "API key",
+                subdomain: "Subdomain (leave this blank if you don't have one)",
+                password: "Password (leave this blank to keep your current password)",
+                password_confirmation: "Confirm the new password"
+            }
+        },
+        cancel_link: "Cancel",
+        log_out_link: "Log out"
     }
 }
