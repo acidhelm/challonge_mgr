e6b05050e21a10b02611902f4b93019a962c2b29
Michael Dunn <acidhelm@gmail.com>
2018-04-29 22:50:58 -0700

    Add names for the routes under "/view", for use in tests

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,11 @@ Rails.application.routes.draw do
             end
         end
     end
-    get "view/:id", to: "tournaments#view"
-    get "view/:id/gold", to: "tournaments#gold"
-    get "view/:id/blue", to: "tournaments#blue"
-    get "view/:id/gold_score", to: "tournaments#gold_score"
-    get "view/:id/blue_score", to: "tournaments#blue_score"
+    get "view/:id", to: "tournaments#view", as: :view_tournament
+    get "view/:id/gold", to: "tournaments#gold", as: :view_tournament_gold
+    get "view/:id/blue", to: "tournaments#blue", as: :view_tournament_blue
+    get "view/:id/gold_score", to: "tournaments#gold_score", as: :view_tournament_gold_score
+    get "view/:id/blue_score", to: "tournaments#blue_score", as: :view_tournament_blue_score
 
     get "/routes", to: redirect("/rails/info/routes") if Rails.env.development?
 end
