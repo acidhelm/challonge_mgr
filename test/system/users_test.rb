b72554975b794bae147cf91e8a957d38ece46c41
Michael Dunn <acidhelm@gmail.com>
2018-05-10 18:20:14 -0700

    Add system tests for the tournament list and user settings pages

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,56 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+    setup do
+        @user = users(:test_user)
+        visit login_url
+
+        fill_in :user_name, with: @user.user_name
+        fill_in :password, with: "password"
+
+        VCR.use_cassette("get_tournament_list") do
+            click_on "Log in"
+            assert_current_path(user_tournaments_path(@user))
+        end
+    end
+
+    test "Check the tournament list" do
+        assert_selector "h1", text: /^Challonge tournaments owned by/
+        assert_selector "a", text: "Reload the tournament list from Challonge"
+        assert_selector "a", text: "Edit this user's settings"
+        assert_selector "a", text: "Log out"
+
+        page.all("th").each do |th|
+            assert th.text.present?
+        end
+
+        page.all("tbody tr").each do |tr|
+            tr.all("td").each_with_index do |td, i|
+                case i
+                    when 0, 1
+                        assert td.text.present?
+                    when 2
+                        assert URI.parse(td.text)
+                    when 3
+                        td.assert_selector "a", text: "Manage this tournament"
+                        td.assert_selector "a", text: "Edit this tournament's settings"
+                end
+            end
+        end
+    end
+
+    test "Check the user settings page" do
+        new_password = "B055man!69"
+
+        visit edit_user_url(@user)
+
+        fill_in "user_api_key", with: "buffythevampireslayer"
+        fill_in "user[subdomain]", with: "drusilla"
+        fill_in "user[password]", with: new_password
+        fill_in "user[password_confirmation]", with: new_password
+
+        click_on "Update User"
+
+        assert_current_path(user_path(@user))
+    end
 end
