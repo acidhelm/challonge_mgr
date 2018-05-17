f334c79c89cae403f076f1eea8b96756cae2164f
Michael Dunn <acidhelm@gmail.com>
2018-05-17 12:55:54 -0700

    Add a system test for the tournaments/show view

--- test/system/tournaments_test.rb
+++ test/system/tournaments_test.rb
@@ -35,4 +35,52 @@ class TournamentsTest < ApplicationSystemTestCase
             assert_current_path(tournament_url)
         end
     end
+
+    test "Check the show-tournament page" do
+        begin
+            # If the user has no tournaments, this call will throw an exception,
+            # but we don't treat that as a test failure.
+            row_elt = page.find("tbody tr:first-child")
+        rescue Capybara::ElementNotFound
+            puts "Warning: The test user has no tournaments. This test is not" \
+                   " running any assertions."
+        end
+
+        if row_elt
+            manage_link = row_elt.find("td:nth-child(4) a:nth-child(1)")
+            manage_url = manage_link["href"]
+            tournament_id = manage_url.match(%r{/tournaments/(\d+)})[1]
+            tournament = @user.tournaments.find(tournament_id)
+
+            VCR.use_cassette("get_tournament_info") do
+                manage_link.click
+
+                # This call has to be within the use_cassette block, because
+                # `click` returns right away.  `assert_selector` spins, looking
+                # for the element, and the HTTP request happens during that loop.
+                assert_selector "h1", text: tournament.name
+            end
+
+            assert_link tournament.challonge_url, href: tournament.challonge_url
+
+            # Since this is using live data from Challonge, we can't predict
+            # what the Upcoming Matches, Completed Matches, and Team Records
+            # sections will contain.  The Team Records section will always exist,
+            # so we can look for that.
+            assert_selector "h2", text: "Team records:"
+            assert_selector "th", text: "Seed"
+            assert_selector "th", text: "Team (W-L)"
+
+            assert_link "Reload this tournament from Challonge",
+                        href: refresh_user_tournament_path(@user, tournament)
+
+            assert_link "Edit this tournament's settings",
+                        href: edit_user_tournament_path(@user, tournament)
+
+            assert_link "Back to the tournament list",
+                        href: user_tournaments_path(@user)
+
+            assert_link "Log Out", href: logout_path
+        end
+    end
 end
