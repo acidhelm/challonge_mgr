289d1a691179c4c308f4fa4535b61182d1a28459
Michael Dunn <acidhelm@gmail.com>
2018-06-04 19:08:26 -0700

    Add the simplecov gem

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development do
 end
 
 group :test do
+  gem "simplecov", require: false
   gem "vcr"
   gem "webmock"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,7 @@
+require "simplecov"
+
+SimpleCov.start "rails"
+
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 
