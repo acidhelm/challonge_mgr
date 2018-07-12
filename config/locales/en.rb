9eff96085cbafc8180e3402e65d461eb297187ad
Michael Dunn <acidhelm@gmail.com>
2018-07-12 09:25:56 -0700

    Update the link on the login page The instructions for creating an account are in INSTALL.md now.

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -35,8 +35,8 @@
                 page_title: "Log in",
                 account_setup_instructions_html: "Enter the user name and password" \
                   " of your Challonge Mgr account. If you haven't made an account" \
-                  " yet, see <a href='%{readme_url}'>the instructions in the readme" \
-                  " file</a> on how to set one up.",
+                  " yet, see <a href='%{url}'>the installation instructions</a>" \
+                  " on how to set one up.",
                 user_name_html: "<u>U</u>ser name",
                 user_name_accesskey: "u",
                 password_html: "<u>P</u>assword",
