f4f434c6d2fb59cbcddb61ca8832934309384322
Michael Dunn <acidhelm@gmail.com>
2018-10-12 11:20:26 -0700

    Add SessionsController tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,9 +6,24 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
     end
 
+    test "Log in" do
+        log_in_as(users(:willow))
+    end
+
     test "Access the logout URL" do
         delete logout_path
         assert_redirected_to root_url
+    end
+
+    test "Try to log in with incorrect credentials" do
+        user = users(:willow)
+
+        post login_path, params: { session: { user_name: user.user_name,
+                                              password: "invalidpassword" } }
+
+        assert_not logged_in?
+        assert_not flash.empty?
+        assert_template "sessions/new"
     end
 
     test "Try to get a user page without logging in" do
