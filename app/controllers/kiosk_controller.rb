287f170e68811a8673b7a3856563a0acd86f4585
Michael Dunn <acidhelm@gmail.com>
2018-12-05 13:45:21 -0800

    Move queries from the kiosk/show view to KioskController

--- app/controllers/kiosk_controller.rb
+++ app/controllers/kiosk_controller.rb
@@ -7,5 +7,9 @@ class KioskController < ApplicationController
         # The user can set the refresh time by passing the time in seconds as
         # the `t` parameter.
         @refresh_time = params[:t] || Rails.configuration.kiosk_refresh_time
+
+        @current_match = @tournament.current_match_obj
+        @on_deck_match = @tournament.matches.upcoming.first
+        @in_the_hole_match = @tournament.matches.upcoming.second
     end
 end
