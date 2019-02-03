fc7c7e7d9745767bccfb0e656830d42850e91323
Michael Dunn <acidhelm@gmail.com>
2019-02-03 11:16:49 -0800

    Move the tournaments/index system test to TournamentsTest Also, explicitly navigate to the index view after logging in.  The app currently always redirects there, but tests shouldn't depend on that behavior.

--- test/system/tournaments_test.rb
+++ test/system/tournaments_test.rb
@@ -3,7 +3,55 @@ require "application_system_test_case"
 class TournamentsTest < ApplicationSystemTestCase
     setup :setup_log_in
 
+    test "Check the tournament list" do
+        visit user_tournaments_path(@user)
+
+        assert_selector "h1", text: /^Challonge tournaments owned by/
+
+        # The footer should have three links.
+        assert_link "Reload the tournament list from Challonge",
+                    href: user_tournaments_refresh_path(@user), exact: true
+        assert_link "Change this user's settings", href: edit_user_path(@user), exact: true
+        assert_link "Log out", href: logout_path, exact: true
+
+        # Check the column headers in the tournament table.
+        header_text = %w(Name State Actions Links)
+
+        page.all("th").each_with_index do |th, i|
+            assert th.text == header_text[i]
+        end
+
+        # Check the list of tournaments.
+        page.all("tbody tr").each do |tr|
+            tr.all("td").each_with_index do |td, i|
+                case i
+                    when 0, 1
+                        # The Name and State columns should have text.
+                        assert td.text.present?
+                    when 2
+                        # The Actions column should have two links.
+                        assert td.has_link? "Manage this tournament", exact: true
+                        assert td.has_link? "Change settings", exact: true
+                    when 3
+                        # The Links column should have three links.
+                        assert td.has_link? "Challonge", exact: true
+                        assert td.has_link? "Spectator view", exact: true
+                        assert td.has_link? "Kiosk", exact: true
+
+                        # Check that the "Challonge" link points to a valid
+                        # Challonge URL.
+                        uri = URI.parse(td.find("a:nth-child(1)")[:href])
+
+                        assert uri.host =~ /^([^.]+\.)?challonge\.com$/
+                        assert uri.path =~ /^\/\w+$/
+                end
+            end
+        end
+    end
+
     test "Check the tournament settings page" do
+        visit user_tournaments_path(@user)
+
         begin
             # If the user has no tournaments, this call will throw an exception,
             # but we don't treat that as a test failure.
@@ -42,6 +90,8 @@ class TournamentsTest < ApplicationSystemTestCase
     end
 
     test "Check the show-tournament page" do
+        visit user_tournaments_path(@user)
+
         begin
             # If the user has no tournaments, this call will throw an exception,
             # but we don't treat that as a test failure.
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,50 +3,6 @@ require "application_system_test_case"
 class UsersTest < ApplicationSystemTestCase
     setup :setup_log_in
 
-    test "Check the tournament list" do
-        assert_selector "h1", text: /^Challonge tournaments owned by/
-
-        # The footer should have three links.
-        assert_link "Reload the tournament list from Challonge",
-                    href: user_tournaments_refresh_path(@user), exact: true
-        assert_link "Change this user's settings", href: edit_user_path(@user), exact: true
-        assert_link "Log out", href: logout_path, exact: true
-
-        # Check the column headers in the tournament table.
-        header_text = %w(Name State Actions Links)
-
-        page.all("th").each_with_index do |th, i|
-            assert th.text == header_text[i]
-        end
-
-        # Check the list of tournaments.
-        page.all("tbody tr").each do |tr|
-            tr.all("td").each_with_index do |td, i|
-                case i
-                    when 0, 1
-                        # The Name and State columns should have text.
-                        assert td.text.present?
-                    when 2
-                        # The Actions column should have two links.
-                        assert td.has_link? "Manage this tournament", exact: true
-                        assert td.has_link? "Change settings", exact: true
-                    when 3
-                        # The Links column should have three links.
-                        assert td.has_link? "Challonge", exact: true
-                        assert td.has_link? "Spectator view", exact: true
-                        assert td.has_link? "Kiosk", exact: true
-
-                        # Check that the "Challonge" link points to a valid
-                        # Challonge URL.
-                        uri = URI.parse(td.find("a:nth-child(1)")[:href])
-
-                        assert uri.host =~ /^([^.]+\.)?challonge\.com$/
-                        assert uri.path =~ /^\/\w+$/
-                end
-            end
-        end
-    end
-
     test "Check the user settings page" do
         new_password = "B055man!69"
 
