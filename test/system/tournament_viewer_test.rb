9acaff464906bb4f0cd9216adec25e8b0fd4b9a5
Michael Dunn <acidhelm@gmail.com>
2019-02-07 00:11:46 -0800

    Flesh out TournamentViewerTest `TournamentViewerTest` now thoroughly checks that the UI is showing the correct data.  In order for it to reliably find tables, I added IDs to some of the <table> tags in the tournaments/show view and the match_list partial.

--- test/system/tournament_viewer_test.rb
+++ test/system/tournament_viewer_test.rb
@@ -1,6 +1,24 @@
 require "application_system_test_case"
 
 class TournamentViewerTest < ApplicationSystemTestCase
+    def upcoming_match_str(match)
+        # U+2014 is &mdash;
+        "#{match.team_name :left} \u2014 #{match.team_name :right}"
+    end
+
+    def completed_match_str(match)
+        # U+2014 is &mdash;
+        "#{match.team_name :left} #{match.team_score :left} \u2014" \
+          " #{match.team_score :right} #{match.team_name :right}"
+    end
+
+    def team_record_str(tournament, team)
+        wins = tournament.matches.winner_is(team).count
+        losses = tournament.matches.loser_is(team).count
+
+        "#{team.name} (#{wins} - #{losses})"
+    end
+
     test "Check the view-tournament page" do
         tournament = tournaments(:tournament_1)
 
@@ -9,19 +27,67 @@ class TournamentViewerTest < ApplicationSystemTestCase
         assert_selector "h1", exact_text: tournament.name
 
         # We're using a made-up tournament from the fixture, so we know that
-        # all three of these sections will be present.
+        # all three of these tables will be present.
         assert_selector "h2", exact_text: "Upcoming matches:"
-        assert_selector "th", exact_text: "Match #"
-        assert_selector "th", exact_text: "Round"
-        assert_selector "th", exact_text: "Teams"
+
+        within "table#upcoming" do
+            within "thead" do
+                [ "Match #", "Round", "Teams" ].each.with_index(1) do |text, i|
+                    assert_selector "th:nth-child(#{i})", exact_text: text
+                end
+            end
+
+            within "tbody" do
+                tournament.matches.upcoming.each.with_index(1) do |match, i|
+                    within "tr:nth-child(#{i})" do
+                        assert_selector "td:first-child", exact_text: match.number.to_s
+                        assert_selector "td:nth-child(2)", exact_text: match.round_name
+                        assert_selector "td:nth-child(3)",
+                                        exact_text: upcoming_match_str(match)
+                    end
+                end
+            end
+        end
 
         assert_selector "h2", exact_text: "Completed matches:"
-        assert_selector "th", exact_text: "Match #"
-        assert_selector "th", exact_text: "Round"
-        assert_selector "th", exact_text: "Teams"
+
+        within "table#completed" do
+            within "thead" do
+                [ "Match #", "Round", "Teams" ].each.with_index(1) do |text, i|
+                    assert_selector "th:nth-child(#{i})", exact_text: text
+                end
+            end
+
+            within "tbody" do
+                tournament.matches.completed.each.with_index(1) do |match, i|
+                    within "tr:nth-child(#{i})" do
+                        assert_selector "td:first-child", exact_text: match.number.to_s
+                        assert_selector "td:nth-child(2)", exact_text: match.round_name
+                        assert_selector "td:nth-child(3)",
+                                        exact_text: completed_match_str(match)
+                    end
+                end
+            end
+        end
 
         assert_selector "h2", exact_text: "Team records:"
-        assert_selector "th", exact_text: "Seed"
-        assert_selector "th", exact_text: "Team (W-L)"
+
+        within "table#team_records" do
+            within "thead" do
+                [ "Seed", "Team (W-L)" ].each.with_index(1) do |text, i|
+                    assert_selector "th:nth-child(#{i})", exact_text: text
+                end
+            end
+
+            within "tbody" do
+                tournament.teams.order(seed: :asc).each.with_index(1) do |team, i|
+                    within "tr:nth-child(#{i})" do
+                        assert_selector "td:first-child", exact_text: team.seed.to_s
+                        assert_selector "td:nth-child(2)",
+                                        exact_text: team_record_str(tournament, team)
+                    end
+                end
+            end
+        end
     end
 end
