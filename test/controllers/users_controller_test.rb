bad6551af265f16c1b193160a88366d3ebc1df56
Michael Dunn <acidhelm@gmail.com>
2019-03-06 00:11:25 -0800

    Write UsersController tests that test creating a demo tournament

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                          password: "B055man69", password_confirmation: "B055man69" } }
     end
 
+    # Uses WebMock to simulate a successful Challonge API call for some of the
+    # steps of creating a demo tournament.
+    def stub_demo_tournament_call(alphanumeric_id, *args)
+        args.each do |arg|
+            case arg
+                when :create
+                    # Since `ChallongeHelper#make_demo_tournament` only reads the
+                    # `url` member of the response (which corresponds to
+                    # `Tournament#challonge_alphanumeric_id`), we can use a simple response.
+                    url = get_api_url
+                    resp = { tournament: { url: alphanumeric_id } }
+
+                when :add
+                    # `ChallongeHelper#make_demo_tournament` doesn't use any of the
+                    # data in the response, so we can use a simple response.
+                    url = get_api_url("#{alphanumeric_id}/participants/bulk_add.json")
+                    resp = [ { participant: {} } ]
+
+                when :start
+                    # Make the call to start the tournament succeed.
+                    # `UsersController#demo` only reads the `url` member of the
+                    # response, so we can use a simple response.
+                    url = get_api_url("#{alphanumeric_id}/start.json")
+                    resp = { tournament: { url: alphanumeric_id } }
+            end
+
+            stub_request(:post, url).to_return(body: resp.to_json)
+        end
+    end
+
     test "Show a user" do
         log_in_as(@user)
         assert logged_in?
@@ -119,6 +149,64 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
         post user_hide_demo_path(@user)
 
         assert_redirected_to login_url
+        assert_not flash.empty?
+    end
+
+    test "Create a demo tournament" do
+        log_in_as(@user)
+        assert logged_in?
+
+        alphanumeric_id = "testdemotournament"
+
+        stub_demo_tournament_call(alphanumeric_id, :create, :add, :start)
+
+        post user_demo_path(@user)
+        assert_redirected_to user_tournaments_refresh_path(@user, autostart: alphanumeric_id)
+        assert flash.empty?
+    end
+
+    test "Try to create a demo tournament, with an API failure when creating the tournament" do
+        log_in_as(@user)
+        assert logged_in?
+
+        stub_request(:post, get_api_url).to_return(make_api_error_response)
+
+        post user_demo_path(@user)
+        assert_redirected_to user_tournaments_path(@user)
+        assert_not flash.empty?
+    end
+
+    test "Try to create a demo tournament, with an API failure when creating teams" do
+        log_in_as(@user)
+        assert logged_in?
+
+        alphanumeric_id = "testdemotournament"
+
+        stub_demo_tournament_call(alphanumeric_id, :create)
+
+        # Make the call to add teams fail.
+        stub_request(:post, get_api_url("#{alphanumeric_id}/participants/bulk_add.json")).
+            to_return(make_api_error_response)
+
+        post user_demo_path(@user)
+        assert_redirected_to user_tournaments_path(@user)
+        assert_not flash.empty?
+    end
+
+    test "Try to create a demo tournament, with an API failure when starting the tournament" do
+        log_in_as(@user)
+        assert logged_in?
+
+        alphanumeric_id = "testdemotournament"
+
+        stub_demo_tournament_call(alphanumeric_id, :create, :add)
+
+        # Make the call to start the tournament fail.
+        stub_request(:post, get_api_url("#{alphanumeric_id}/start.json")).
+            to_return(make_api_error_response)
+
+        post user_demo_path(@user)
+        assert_redirected_to user_tournaments_path(@user)
         assert_not flash.empty?
     end
 end
