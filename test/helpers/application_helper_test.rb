fbb0efc4bc546e41f0af363f9d1861612c4f17fb
Michael Dunn <acidhelm@gmail.com>
2019-04-30 12:07:53 -0700

    Add tests for ApplicationHelper#validate_param Test that it works properly when it's passed a block.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ApplicationHelperTest < ActionView::TestCase
     include ApplicationHelper
 
-    test "Test parameter validation" do
+    test "Test parameter validation, without a block" do
         legal_values = %i(buffy willow)
 
         assert_nothing_raised do
@@ -12,6 +12,22 @@ class ApplicationHelperTest < ActionView::TestCase
 
         assert_raises(ArgumentError) do
             validate_param(:alyson, legal_values)
+        end
+    end
+
+    test "Test parameter validation, with a block" do
+        assert_nothing_raised do
+            validate_param(:tara) do |sym|
+                assert_equal :tara, sym
+                true
+            end
+        end
+
+        assert_raises(ArgumentError) do
+            validate_param(:faith) do |sym|
+                assert_equal :faith, sym
+                false
+            end
         end
     end
 end
